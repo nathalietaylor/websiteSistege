42b56ac543ff774df44243867150804044c82f25
Nathalie Taylor <nmtzampieri@gmail.com>
2025-06-18 21:04:07 -0300

    commit inicial

---
+++ sistege/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="bg-dark text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-primary">
+        Tailwind está funcionando!
+      </h1>
+    </div>
+  );
+}
---
+++ sistege/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#800020",
+        "primary-dark": "#66001a",
+        dark: "#121212",
+      },
+    },
+  },
+  plugins: [],
+};
